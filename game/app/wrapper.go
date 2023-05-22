8ed2d1a40d48d15ffccf9721770a4d6e71a537ac
joohnes <kubapienkowski@gmail.com>
2023-05-22 11:19:55 +0200

    added wrapper for server errors

---
+++ game/app/wrapper.go
@@ -0,0 +1,26 @@
+package app
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+const maxConnectionTries int = 10
+
+func ServerErrorWrapper(f func() error) error {
+	log := GetLoggerInstance()
+	for x := 0; x < maxConnectionTries; x++ {
+		err := f()
+		if err == nil {
+			return nil
+		}
+		if err != nil && showErrors {
+			log.Println(err)
+		}
+		fmt.Printf("#%d - Server error occured. Please wait\n", x+1)
+		log.Printf("#%d - Server error occured. Please wait\n", x+1)
+		time.Sleep(1 * time.Second)
+	}
+	return errors.New("max connection tries reached")
+}
