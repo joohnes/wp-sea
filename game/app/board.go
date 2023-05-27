5eb7fc67e875a2e720fd82a1fab7a57f10507607
joohnes <kubapienkowski@gmail.com>
2023-05-28 01:04:28 +0200

    added shots/hits/performance display on board

--- game/app/board.go
+++ game/app/board.go
@@ -9,31 +9,49 @@ import (
 
 func (a *App) ShowBoard(ctx context.Context, coordchan chan<- string, textchan <-chan string, errorchan <-chan error, timeLeftchan <-chan int) {
 	ui := gui.NewGUI(true)
-	txt := gui.NewText(1, 1, "Press Ctrl+C to exit", nil)
-	ui.Draw(txt)
-	myBoard := gui.NewBoard(1, 4, nil)
-	ui.Draw(myBoard)
-	enemyBoard := gui.NewBoard(50, 4, nil)
-	ui.Draw(enemyBoard)
+	Left := 1
+	Right := 50
+
+	txt := gui.NewText(Left, 1, "Press Ctrl+C to exit", nil)
+	myBoard := gui.NewBoard(Left, 6, nil)
+	enemyBoard := gui.NewBoard(Right, 6, nil)
 
 	//TEXTS
-	timer := gui.NewText(1, 2, "Timer: ", nil)
-	ui.Draw(timer)
-	playerNick := gui.NewText(1, 26, fmt.Sprintf("Player: %s", a.nick), nil)
-	playerDesc := gui.NewText(1, 27, fmt.Sprintf("Player's desc: %s", a.desc), nil)
-	oppNick := gui.NewText(1, 28, fmt.Sprintf("Opp: %s", a.oppNick), nil)
-	oppDesc := gui.NewText(1, 29, fmt.Sprintf("Opp's desc: %s", a.oppDesc), nil)
-	ui.Draw(playerNick)
-	ui.Draw(playerDesc)
-	ui.Draw(oppNick)
-	ui.Draw(oppDesc)
+	timer := gui.NewText(Left, 2, "Timer: ", nil)
+	playerNick := gui.NewText(Left, 29, fmt.Sprintf("Player: %s", a.nick), nil)
+	playerDesc := gui.NewText(Left, 30, fmt.Sprintf("Player's desc: %s", a.desc), nil)
+	oppNick := gui.NewText(Left, 31, fmt.Sprintf("Opp: %s", a.oppNick), nil)
+	oppDesc := gui.NewText(Left, 32, fmt.Sprintf("Opp's desc: %s", a.oppDesc), nil)
 
-	turnText := gui.NewText(50, 1, "", nil)
-	ui.Draw(turnText)
-	chanText := gui.NewText(1, 31, "test", nil)
-	ui.Draw(chanText)
-	errorText := gui.NewText(1, 32, "error", nil)
-	ui.Draw(errorText)
+	turnText := gui.NewText(Right, 4, "", nil)
+	chanText := gui.NewText(Left, 34, "test", nil)
+	errorText := gui.NewText(Left, 35, "error", nil)
+
+	shotsCounttxt := gui.NewText(Right, 1, "Shots: 0", nil)
+	shotsHittxt := gui.NewText(Right, 2, "Hit: 0", nil)
+	performancetxt := gui.NewText(Right, 3, "Performance: %", nil)
+
+	DrawList := func(a ...gui.Drawable) {
+		for _, d := range a {
+			ui.Draw(d)
+		}
+	}
+	DrawList(
+		txt,
+		myBoard,
+		enemyBoard,
+		timer,
+		turnText,
+		chanText,
+		errorText,
+		playerNick,
+		playerDesc,
+		oppNick,
+		oppDesc,
+		shotsHittxt,
+		shotsCounttxt,
+		performancetxt,
+	)
 
 	myBoard.SetStates(a.myStates)
 	enemyBoard.SetStates(a.enemyStates)
@@ -67,6 +85,7 @@ func (a *App) ShowBoard(ctx context.Context, coordchan chan<- string, textchan <
 				turnText.SetText("Your Turn!")
 			case StateOppTurn:
 				turnText.SetText("Enemy's turn!")
+
 			}
 		}
 	}()
@@ -76,6 +95,13 @@ func (a *App) ShowBoard(ctx context.Context, coordchan chan<- string, textchan <
 			time.Sleep(200 * time.Millisecond)
 			myBoard.SetStates(a.myStates)
 			enemyBoard.SetStates(a.enemyStates)
+			shotsCounttxt.SetText(fmt.Sprintf("Shots: %d", a.shotsCount))
+			shotsHittxt.SetText(fmt.Sprintf("Hits: %d", a.shotsHit))
+			var performance float64
+			if a.shotsCount != 0 {
+				performance = float64(a.shotsHit) / float64(a.shotsCount) * 100
+			}
+			performancetxt.SetText(fmt.Sprintf("Performance: %.2f%", performance))
 		}
 	}()
 
@@ -85,11 +111,13 @@ func (a *App) ShowBoard(ctx context.Context, coordchan chan<- string, textchan <
 
 func (a *App) SetUpShips(ctx context.Context, shipchannel chan string, errorchan chan error) {
 	ui := gui.NewGUI(true)
-	txt := gui.NewText(1, 1, "Press Ctrl+C to exit", nil)
+	Left := 1
+	//Right := 50
+	txt := gui.NewText(Left, 1, "Press Ctrl+C to exit", nil)
 	ui.Draw(txt)
-	myBoard := gui.NewBoard(1, 4, nil)
+	myBoard := gui.NewBoard(Left, 4, nil)
 	ui.Draw(myBoard)
-	errorText := gui.NewText(1, 32, "error", nil)
+	errorText := gui.NewText(Left, 32, "error", nil)
 	ui.Draw(errorText)
 	myBoard.SetStates(a.myStates)
 	go func() {
